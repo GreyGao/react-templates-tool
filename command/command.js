bf88071eb8e405c63c2ad8862fad1a69a9bebb5a
Grey Gao <rh87116938@163.com>
2019-04-18 16:52:27 +0800

    optimized async function and process.

--- command/command.js
+++ command/command.js
@@ -4,31 +4,32 @@ const path = require('path')
 const files = require('../lib/files')
 const fs = require('fs')
 const rimraf = require('rimraf')
+const util = require('util');
+
+const rimrafAsync = util.promisify(rimraf)
 
 module.exports = {
   create: (type, projectName) => {
-    console.log('get the latest repositories ...')
-    git
-      // 1. clone the latest version templates to local path : /templates
-      .clone(`${repositories[type].url}`, `../templates/${type}`)
-      .exec(
-        () => {
-          console.log('begin to make copy...')
+    try {
+      console.log('get the latest repositories ...')
+      git
+        // 1. clone the latest version templates to local path : /templates
+        .clone(`${repositories[type].url}`, `../templates/${type}`)
+        .exec(
+          async () => {
+            console.log('begin to make copy...')
 
-          // 2. copy dirs to command execute path
-          const filePath = path.resolve(__dirname, `../templates/${type}/`)
-          files.makeCopy(filePath, projectName)
+            // 2. copy dirs to command execute path
+            const filePath = path.resolve(__dirname, `../templates/${type}/`)
+            await files.makeCopyAsync(filePath, projectName)
 
-          // 3. clear templates folder
-          rimraf(filePath, err => {
-            if (err) {
-              console.log(err)
-            } else {
-              console.log('construction done')
-            }
-          })
-        }
-      )
-
+            // 3. clear templates folder
+            await rimrafAsync(filePath)
+            console.log('construction done')
+          }
+        )
+    } catch (err) {
+      console.log(err)
+    }
   }
 }
